48aace360e6ceb0df9f54b5b28ae278ff9ffb936
auroraptor <interestingbutno@yandex.ru>
2022-11-25 12:20:42 +0300

    feat: добавить фильтры user

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,6 @@ const { HttpStatusCode } = require('../utils/HttpStatusCode');
 const { HTTP401Error } = require('../errors/HTTP401Error');
 const { HTTP409Error } = require('../errors/HTTP409Error');
 const { HTTP404Error } = require('../errors/HTTP404Error');
-const { logNow } = require('../utils/log');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -15,9 +14,12 @@ module.exports.createUser = async (req, res, next) => {
     const hash = await bcrypt.hash(req.body.password, 10);
     const user = await User.create({ ...req.body, password: hash });
     const {
-      name, email, _id,
+      name, email, _id: id,
     } = user;
-    res.status(HttpStatusCode.OK).send({ name, email, id: _id });
+
+    res.status(HttpStatusCode.OK).send({
+      user: { name, email, id },
+    });
   } catch (error) {
     if (error.name === 'MongoServerError' || error.message.includes('11000')) {
       next(new HTTP409Error(`${req.body.email} уже зарегестрирован`));
@@ -29,8 +31,19 @@ module.exports.createUser = async (req, res, next) => {
 
 module.exports.getUsers = async (req, res, next) => {
   try {
-    const users = await User.find({});
-    res.send({ users });
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize, 10) : 0;
+    const page = req.query.page ? parseInt(req.query.page, 10) : 0;
+    const { name, email } = req.query;
+    const filter = {};
+
+    if (name) filter.name = name;
+    if (email) filter.email = email;
+
+    const data = await User.find(filter).limit(pageSize).skip(pageSize * page);
+    const users = data.map((item) => ({
+      name: item.name, email: item.email, id: item._id,
+    }));
+    res.send(users);
   } catch (error) {
     next(error);
   }
@@ -38,15 +51,16 @@ module.exports.getUsers = async (req, res, next) => {
 
 module.exports.getCurrentUser = async (req, res, next) => {
   try {
-    logNow('USER REQUEST: ', req.user);
     const user = await User.findById(req.user._id);
     if (!user) {
       next(new HTTP404Error(`Пользователь с id ${req.user._id} не найден`));
       return;
     }
-    const { name, email, _id } = user;
+    const { name, email, _id: id } = user;
 
-    res.status(HttpStatusCode.OK).send({ name, email, id: _id });
+    res.status(HttpStatusCode.OK).send({
+      user: { name, email, id },
+    });
   } catch (error) {
     next(error);
   }
@@ -58,9 +72,11 @@ module.exports.updateUser = async (req, res, next) => {
       new: true,
       runValidators: true,
     });
-    const { name, email, _id } = user;
+    const { name, email, _id: id } = user;
 
-    res.send({ name, email, id: _id });
+    res.status(HttpStatusCode.OK).send({
+      user: { name, email, id },
+    });
   } catch (error) {
     next(error);
   }
